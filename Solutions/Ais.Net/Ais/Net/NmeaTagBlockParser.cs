30f20d7febe3497ba004cc8273ede733578a8337
Ian Griffiths <Ian.Griffiths@endjin.com>
2024-11-13 10:13:00 +0000

    Get building on .NET 7.0 SDK

--- Solutions/Ais.Net/Ais/Net/NmeaTagBlockParser.cs
+++ Solutions/Ais.Net/Ais/Net/NmeaTagBlockParser.cs
@@ -60,7 +60,7 @@ namespace Ais.Net
                 switch (fieldType)
                 {
                     case 'g':
-                        this.SentenceGrouping = this.ParseSentenceGrouping(ref source);
+                        this.SentenceGrouping = ParseSentenceGrouping(ref source);
                         break;
 
                     case 's':
@@ -105,7 +105,13 @@ namespace Ais.Net
                         break;
                 }
 
-                static ReadOnlySpan<byte> AdvanceToNextField(ref ReadOnlySpan<byte> source)
+                // Adding scoped here keeps the compiler happy, but it's not clear what additional
+                // powers this method would have had without the scoped keyword. It's not like
+                // we're *not* making it escape: we return a span derived from source! It's possible
+                // that the only effect here is that it changes the effective safe-to-escape of
+                // the return value. Without the scoped, it might be that the return value would
+                // be treated as more escapable?
+                static ReadOnlySpan<byte> AdvanceToNextField(scoped ref ReadOnlySpan<byte> source)
                 {
                     ReadOnlySpan<byte> result;
                     int next = source.IndexOf((byte)',');
@@ -140,7 +146,9 @@ namespace Ais.Net
         /// </summary>
         public long? UnixTimestamp { get; }
 
-        private static bool GetEnd(in ReadOnlySpan<byte> source, char? delimiter, out ReadOnlySpan<byte> remaining, out int length)
+        // As with AdvanceToNextField above, I can't see what limits scoped is imposing on this
+        // method, but the compiler isn't happy unless we add it.
+        private static bool GetEnd(scoped in ReadOnlySpan<byte> source, char? delimiter, out ReadOnlySpan<byte> remaining, out int length)
         {
             if (delimiter.HasValue)
             {
@@ -213,7 +221,37 @@ namespace Ais.Net
             return true;
         }
 
-        private NmeaTagBlockSentenceGrouping ParseSentenceGrouping(ref ReadOnlySpan<byte> source)
+        // This needed to change to a static member under the C# 11.0 rules.
+        // Why is that?
+        // Rather surprisingly, it's because starting with C# 11.0, you can initialize spans with
+        // references to local variables. But it's not remotely obvious why that means this method
+        // became unacceptable.
+        // The basic problem is that as an instance member, it is able to replace the caller's
+        // ReadOnlySpan<byte> with one that it captured earlier, e.g., during construction.
+        // Under the design of the ref safety rules in C# 7.2, this didn't present any problems,
+        // so it was allowed. But the rules have changed in C# 11.0 to accommodate the ability to
+        // create spans that refer to local variables, as described in
+        //  https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-11.0/low-level-struct-improvements#compat-considerations
+        // (And more generally, ref inputs can be captured as ref fields, although it appears that
+        // the compiler currently imposes stricter constraints on this than it does for spans, so
+        // at the moment, you need to use spans to run into the problem.) When it was impossible
+        // for a span to refer to a local variable, there was no possibility for a ref argument
+        // to be promoted to some ref struct that held onto that ref. But now that this is
+        // possible, the compiler has to be more conservative about which calls it allows to
+        // instance methods of ref structs which accept ref or out arguments of ref struct type.
+        // It's still not 100% clear to me exactly how the rule changes have worked here, but I
+        // think  it might be something to do with the "Method Arguments Must Match" rule,
+        // which used to look like this:
+        //  https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-7.2/span-safety#method-arguments-must-match
+        // and has been changed thus:
+        //  https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-11.0/low-level-struct-improvements#method-arguments-must-match
+        // It's also possible that the "scoped" nature of "this" has had an impact. C# used to
+        // consider the implicit 'this' of a ref struct to be a 'ref MyStructType', but in C# 11.0
+        // this changed to 'scoped ref MyStructType', and it's possible that that's coming into
+        // play here.
+        // As far as I can tell, the fact that we are calling this from inside the constructor
+        // also seems to have some impact.
+        private static NmeaTagBlockSentenceGrouping ParseSentenceGrouping(ref ReadOnlySpan<byte> source)
         {
             if (!ParseDelimitedInt(ref source, out int sentenceNumber, '-'))
             {
